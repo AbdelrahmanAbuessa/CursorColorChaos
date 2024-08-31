a734fcd7914210c36475437313933e60fc52f59e
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-08-31 23:12:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let main_screen = document.getElementById("main");
 let isntructions = document.getElementById("instructions");
 let leaders = document.getElementById("leaderboard");
 let ground = document.getElementById("ground");
+let announceText = document.getElementById("announceText");
+let colorBlock = document.getElementById("colorBlock");
 
 main_screen.style.display = "block";
 isntructions.style.display = "none";
 leaders.style.display = "none"
 ground.style.display = "none"
 
-let difficulty = 0;
+let level = 0;
+let game = false;
+
+let loss = false;
 
 let colors = ["black", "white", "gray", "lightgray", "red", "aqua", "blue", "brown", "cyan", "darkblue", "darkgreen", "darkgrey", "darkorange", "darkred", "green", "lime", "lightblue", "lightgreen", "lightyellow", "magenta", "maroon", "navy", "orange", "pink", "purple", "red", "yellow"];
-let primaryColors = ["red", "blue", "black", "white", "green", "purple", "red", "yellow"];
+let primaryColors = ["red", "blue", "black", "white", "green", "purple", "pink", "red", "yellow"];
 
 document.addEventListener("click", function (e) {
     let targetElement = e.target;
@@ -20,9 +25,11 @@ document.addEventListener("click", function (e) {
     } else if (targetElement.id === "lead") {
         leaderboard();
     } else if (targetElement.id === "start") {
-        startGame();
+        loadGame();
     } else if (targetElement.id === "return") {
         returnToMenu();
+    } else if (targetElement.id === "colorBlock") {
+        startGame();
     }
 })
 
@@ -44,9 +51,42 @@ function returnToMenu() {
     isntructions.style.display = "none";
 }
 
-function startGame() {
+function loadGame() {
     main_screen.style.display = "none";
     leaders.style.display = "none"
     isntructions.style.display = "none";
     ground.style.display = "block"
+
+    if (!game) {
+        level = 1;
+        let index = Math.floor(Math.random() * 10);
+        if (index === 0) {
+            index = 1;
+        }
+        let start_color = primaryColors[index - 1];
+        announceText.innerText = "Hover over the Square to start the game";
+        colorBlock.style.backgroundColor = start_color;
+    } else {
+        level += 1;
+        startGame();
+    }
 }
+
+function startGame() {
+    announceText.innerText = "The Color is:";
+    let index = Math.floor(Math.random() * 10);
+    if (index === 0) {
+        index = 1;
+    }
+    let color = primaryColors[index - 1];
+    colorBlock.style.backgroundColor = color;
+}
+
+// loadd starter game
+// when click over square: set level & dificulty
+// update game, load squares, select random colors, apply them and squares and stuff
+// set timeout, check if mouse is hovering over correct square
+// if hover, proceed increase level and difficulty
+// number of squares and number of colors increase with level and difficulty according to a chart
+
+// if not hover then end game 
