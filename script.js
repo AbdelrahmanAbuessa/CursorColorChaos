8eda63ba47fef56aef0e62214a73462bb8dd28db
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-08-31 22:23:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let main_screen = document.getElementById("main");
+let isntructions = document.getElementById("instructions");
+let leaders = document.getElementById("leaderboard");
+let ground = document.getElementById("ground");
+
+main_screen.style.display = "block";
+isntructions.style.display = "none";
+leaders.style.display = "none"
+ground.style.display = "none"
+
+let difficulty = 0;
+
+let colors = ["black", "white", "gray", "lightgray", "red", "aqua", "blue", "brown", "cyan", "darkblue", "darkgreen", "darkgrey", "darkorange", "darkred", "green", "lime", "lightblue", "lightgreen", "lightyellow", "magenta", "maroon", "navy", "orange", "pink", "purple", "red", "yellow"];
+let primaryColors = ["red", "blue", "black", "white", "green", "purple", "red", "yellow"];
+
 document.addEventListener("click", function (e) {
     let targetElement = e.target;
     if (targetElement.id === "how") {
@@ -6,5 +21,32 @@ document.addEventListener("click", function (e) {
         leaderboard();
     } else if (targetElement.id === "start") {
         startGame();
+    } else if (targetElement.id === "return") {
+        returnToMenu();
     }
 })
+
+function leaderboard() {
+    leaders.style.display = "block"
+    main_screen.style.display = "none";
+    isntructions.style.display = "none";
+}
+
+function howToPlay() {
+    leaders.style.display = "none"
+    main_screen.style.display = "none";
+    isntructions.style.display = "flex";
+}
+
+function returnToMenu() {
+    main_screen.style.display = "block";
+    leaders.style.display = "none"
+    isntructions.style.display = "none";
+}
+
+function startGame() {
+    main_screen.style.display = "none";
+    leaders.style.display = "none"
+    isntructions.style.display = "none";
+    ground.style.display = "block"
+}
